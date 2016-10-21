05dde4958faad6ae0778365ea2ac92072692f4a4
Tehnikumi Õpilane <opilane@tptlive.ee>
2016-10-21 11:01:35 +0300

    Initial commit

---
+++ src/exercise42.java
@@ -0,0 +1,16 @@
+/**
+ * Created by opilane on 21.10.2016.
+ */
+public class exercise42 {
+    public static void zool() {
+        int number = 11;
+        String pet = "Max ";
+        String street = "Metsa";
+        System.out.print("Tulemus on: " + number + pet + street);
+    }
+    public static void main (String[] args) {
+        zool();
+
+
+    }
+}
